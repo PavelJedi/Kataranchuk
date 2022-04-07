15e4cf1c2e65ff946ea74e39d371213fcccf0175
Pavlo_Kataranchuk <pavel83838300@gmail.com>
2022-04-07 17:56:07 +0300

    init

---
+++ lesson25_js/src/js/main.js
@@ -0,0 +1,11 @@
+<!--Correct variables-->
+let name;
+const lastName = 'Kataranchuk';
+let fullName = 'Pavlo Kataranchuk';
+let mobile_number = 123;
+const person = fullName;
+
+// Incorrect variables
+// let Name;
+// let 123person;
+// const FULL-NAME = 'smth';

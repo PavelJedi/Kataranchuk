c3ef97ec75455314dddd7bd1d6940646891c746d
Pavlo_Kataranchuk <pavel83838300@gmail.com>
2022-04-08 21:12:48 +0300

    init

---
+++ lesson26/src/js/main.js
@@ -0,0 +1,33 @@
+let obj = {
+    name:'Pavlo',
+    lastName:'Kataranchuk',
+    hobbies: 'Traveling',
+    age: 24,
+    phone: +380509998535,
+    language: 'ukranian',
+    city: 'Chernivtsi'
+};
+
+let friend = {
+    name: 'Kate',
+    hairColor: 'pink',
+    age: 24,
+    city: 'marsel',
+    nationality: 'france',
+    hobbies: 'Traveling',
+    language: 'French'
+};
+
+console.log(friend.name == obj.name);
+console.log(friend.hairColor == obj.lastName);
+console.log(friend.age == obj.age);
+console.log(friend.hobbies == obj.hobbies);
+console.log(friend.city == obj.city);
+console.log(friend.language == obj.language);
+console.log(friend.nationality == obj.phone);
+
+
+
+
+
+
